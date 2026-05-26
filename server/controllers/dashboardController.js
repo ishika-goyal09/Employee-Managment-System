fd8a0fbcf04054cbd2d02820cb38710f0476c1e1
Ishika Goyal <ishikagoyal336@gmail.com>
2026-05-26 19:05:11 +0530

    Api created for Dashboard data

---
+++ server/controllers/dashboardController.js
@@ -0,0 +1,67 @@
+import { DEPARTMENTS } from "../constants/departments.js";
+import Attendance from "../modals/Attendance.js";
+import Employee from "../modals/Employee.js";
+import leaveApplication from "../modals/LeaveApplication.js";
+import Payslip from "../modals/Payslip.js";
+
+//Get dashboard for employee and admin
+// GET/api/dashboard
+export const getDashboard = async(req,res)=>{
+    try{
+        const session = req.session;
+        if(session.role === "ADMIN"){
+            const [totalEmployees,totalAttendance,pendingLeaves] = await
+            Promise.all([
+                Employee.countDocuments({isDeleted:{ $ne:true}}),
+                Attendance.countDocuments({
+                    date:{
+                        $gte:new Date(new Date().setHours(0,0,0,0)),
+                        $li:new Date(new Date().setHours(0,0,0,0)),
+                    }
+                }),
+                leaveApplication.countDocuments({status:"PENDING"})
+            ])
+            return res.json({
+                role:"ADMIN",
+                totalEmployees,
+                totalDepartments:DEPARTMENTS.length,
+                todayAttendance,
+                pendingLeaves
+            })
+        } else{
+            const employee = await Employee.findOne({
+                userId:session.userId,
+            }).lean();
+            if(!employee) return res.status(404).json({error:"Employee not found"})
+             
+                const today = new Date();
+                const [currentMonthAttendance,pendingLeaves,latestPayslip] = await Promise.all([
+                    Attendance.countDocuments({
+                        employeeId:employee._id,
+                        date:{
+                        $gte:new Date(today.getFullYear(),today.getMonth(),1),
+                        $lt:new Date(today.getFullYear(),today.getMonth() + 1, 1),
+                        }
+                    }),
+                    leaveApplication.countDocuments({
+                        employeeId:employee._id,
+                        status:"PENDING",
+                    }),
+                    Payslip.findOne({ employee:employee._id}).sort({
+                        createAt: -1 }).lean(),
+                ])
+                return res.json({
+                    role:"EMPLOYEE",
+                    employee:{...employee,id: employee._id.toString()},
+                    currentMonthAttendance,
+                    pendingLeaves,
+                    latestPayslip:latestPayslip ? {...latestPayslip,id:
+                    latestPayslip._id.toString()} : null
+                    
+                })
+        }
+    } catch (error){
+        console.error("Dashboard error:",error)
+        return res.status(500).json({error:"Failed"})
+    }
+}
